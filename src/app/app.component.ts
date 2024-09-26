67983d345d575393e1492706f70ea64bffd4e999
Mahi Prajapati <w0846078myscc.ca>
2024-09-25 23:48:12 -0400

    Finished by setting display code and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Noodles} from "./modules/noodles";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Assignment1';
+  title = 'Noodles Shop';
 
-  id = 123;
-  firstName ='Mahi';
-
+  noodlesList: Noodles[] = [
+    {name: 'Ramen', type: 'Wheat', origin: 'Japan', isSpicy: true, price: 8.99},
+    {name: 'Pho', type: 'Rice', origin: 'vietnam', isSpicy: false, price: 9.99},
+    {name: 'Pad Thai', type: 'Rice', origin: 'Thailand', isSpicy: true, price: 11.99},
+    {name: 'Soba', type: 'Buckwheat', origin: 'Japan', isSpicy: false, price: 7.99},
+    {name: 'Lo Mein',type: 'Wheat', origin: 'China', isSpicy: false, price: 10.99},
+    {name: 'Spicy Korean Noodles', type: 'Wheat', origin: 'Korea', isSpicy: true, price: 12.99}
+  ]
 }

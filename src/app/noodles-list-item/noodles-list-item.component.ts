9fa47fc3c16e8d21e9f01f9f37c537193cb0d01a
Mahi Prajapati <w0846078myscc.ca>
2024-09-26 01:44:47 -0400

    finished by updating styles and steps

--- src/app/noodles-list-item/noodles-list-item.component.ts
+++ src/app/noodles-list-item/noodles-list-item.component.ts
@@ -1,9 +1,12 @@
 import {Component, input, Input} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-noodles-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './noodles-list-item.component.html',
   styleUrl: './noodles-list-item.component.css'
 })

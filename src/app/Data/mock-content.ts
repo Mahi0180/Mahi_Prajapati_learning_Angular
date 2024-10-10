7532524b8e021d5ba8f9a4354eb729a7d947f129
Mahi Prajapati <w0846078@myscc.ca>
2024-10-09 22:27:07 -0400

    created a folder named-services and data

---
+++ src/app/Data/mock-content.ts
@@ -0,0 +1,9 @@
+import {Noodles} from "../models/noodles";
+
+
+export const NoodlesList: Noodles[] =[
+    { name:'Ramen', price: 5.00 },
+    { name:'Soba', price: 6.00 },
+    { name:'Udon', price: 7.00},
+    { name:'Spaghetti', price: 4.50}
+  ];

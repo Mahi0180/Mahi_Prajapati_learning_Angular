69ef4d7617d0eb31511cd619ff74ac4bbf7a1f25
Mahi Prajapati <w0846078@myscc.ca>
2024-11-21 18:23:23 -0500

    tried to add the bonus code -show details on hover but unable to do that due to some errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,15 @@ import {NoodlesListComponents} from "./noodles-list/noodles-list.component";
 import {HoverHighlightDirective} from "./hover-highlight.directive";
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {ModifyListItemComponent} from "./modify-list-item/modify-list-item.component";
+import {HighlightOnFocusDirective} from "./highlight-on-focus.directive";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css',
-  imports: [NoodlesListComponents, ModifyListItemComponent, HoverHighlightDirective, RouterOutlet, RouterLink]
+  imports: [NoodlesListComponents, ModifyListItemComponent, HoverHighlightDirective, RouterOutlet, RouterLink, HighlightOnFocusDirective]
 })
 export class AppComponent {
-
-  title = 'Assignment4';
 
 }
